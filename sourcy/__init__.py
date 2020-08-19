ea6da3b63ac4d58dab8e1e9ff018a8837f51a319
Cezar Sas <cezar.sas@gmail.com>
2020-08-19 18:16:34 +0200

    Bump version: 0.1.7 → 0.1.8

--- sourcy/__init__.py
+++ sourcy/__init__.py
@@ -2,7 +2,7 @@ from sourcy.language import Language
 
 __author__ = """Cezar Sas"""
 __email__ = 'cezar.sas@gmail.com'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 
 def load(lang: str, *args, **kwargs) -> Language:
